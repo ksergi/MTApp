0f81d5c3afaf839ab7b3e8dbd6aae768e04e455f
Sergi Kuhs <sergi.kuhs@gmail.com>
2012-04-01 12:13:08 +0200

    added

---
+++ arealvis/xmlParser.h
@@ -0,0 +1,16 @@
+//
+//  xmlParser.h
+//  parser
+//
+//  Created by Sergio Kuhs on 22.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface xmlParser : NSObject
+
+-(NSArray *)withXPath:(NSString *) xPath;
+
+
+@end
